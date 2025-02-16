e4e497ae1ad73022917f86d39ff729e6b28b8d58
Oscar Pavon <pavon_oscar@outlook.com>
2025-02-16 00:03:53 +0000

    updated generate commands

--- generate_compile_commands.sh
+++ generate_compile_commands.sh
@@ -1,3 +1,3 @@
-#!/bin/sh
-make -j32 | grep -wE 'cc' | grep -w '\-c' | jq -nR '[inputs|{directory:".", command:., file: match(" [^ ]+$").string[1:]}]'  > compile_commands.json
+#!/bin/bash
+/usr/bin/make --always-make --dry-run | grep -wE 'cc' | grep -w '\-c' | jq -nR '[inputs|{directory:".", command:., file: match(" [^ ]+$").string[1:]}]'  > compile_commands.json
 
